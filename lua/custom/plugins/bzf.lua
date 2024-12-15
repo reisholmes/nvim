ecca12e59711cd8b3f00da64f12c20f65f30db5b
reisholmes <reisholmes@gmail.com>
2024-12-15 23:55:24 +0100

    add bzf and pqf plugins

---
+++ lua/custom/plugins/bzf.lua
@@ -0,0 +1,14 @@
+-- https://github.com/folke/trouble.nvim
+return {
+  -- QUICKFIX IMPROVEMENTS
+  --
+  -- <Tab> to select items.
+  -- zn to keep selected items.
+  -- zN to filter selected items.
+  -- zf to fuzzy search items.
+  --
+  -- <Ctrl-f> scroll down
+  -- <Ctrl-b> scroll up
+  'kevinhwang91/nvim-bqf',
+  ft = 'qf',
+}
